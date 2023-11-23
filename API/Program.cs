365c1f3c30972c079c385bbcd08af7a36fadf22f
FriptuT <friptut@gmail.com>
2023-11-23 19:54:15 +0200

    fetching data from backend

--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,8 @@ internal class Program
             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
 
+        builder.Services.AddCors();
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -27,6 +29,10 @@ internal class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.UseCors(options => {
+            options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
 
         app.UseHttpsRedirection();
 
